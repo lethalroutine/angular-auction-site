f80f2911b5883b0eb3953c8ac87f2309c83983df
aleksander.gajda <aleksander.gajda@selvita.com>
2020-01-30 13:48:30 +0100

    Added dynamic generation of auction cards

--- src/app/auction/auction-card/auction-card.component.ts
+++ src/app/auction/auction-card/auction-card.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { AuctionItem } from './../auctions-site/auction-item';
 
 @Component({
   selector: 'ap-auction-card',
   template: `
-    <p>
-      auction-card works!
-    </p>
+  <div class="card">
+  <div class="card-header">
+    {{ auction.title }}
+    <img [src]='auction.imageUrl' class="card-img">
+  </div>
+  <div class="card-body">
+    <p class="card-text">{{ auction.description }}</p>
+    <div class="d-flex justify-content-between">
+      <strong>{{ auction.price }} zł</strong>
+      <button class="btn btn-primary">
+        <i class="fa fa-cart-plus"></i>
+      </button>
+    </div>
+  </div>
+</div>
   `,
   styles: []
 })
 export class AuctionCardComponent implements OnInit {
+  @Input() auction: AuctionItem;
 
   constructor() { }
 
--- src/app/auction/auctions-site/auctions-site.component.ts
+++ src/app/auction/auctions-site/auctions-site.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuctionItem } from './auction-item';
 
 @Component({
   selector: 'ap-auctions-site',
@@ -6,6 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class AuctionsSiteComponent implements OnInit {
+  auctionItems: AuctionItem[] = [
+    {
+      id: 1,
+      imageUrl: 'https://picsum.photos/id/3/200/200',
+      title: 'iphone 3g',
+      description: 'ajfon',
+      price: 100
+    },
+    {
+      id: 2,
+      imageUrl: 'https://picsum.photos/id/2/200/200',
+      title: 'samsung s5',
+      description: 'samsung',
+      price: 250
+    }
+  ];
 
   constructor() { }
 
