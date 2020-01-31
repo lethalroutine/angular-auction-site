140beb1eb37ed1d8abc6d317ceb8ca4fe5aeb372
aleksander.gajda <aleksander.gajda@selvita.com>
2020-01-31 09:23:34 +0100

    Add add-auction-site comopnent

---
+++ src/app/auction/add-auction-site/add-auction-site.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ap-add-auction-site',
+  templateUrl: './add-auction-site.component.html',
+  styleUrls: ['./add-auction-site.component.css']
+})
+export class AddAuctionSiteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/auction/auction.module.ts
+++ src/app/auction/auction.module.ts
@@ -7,10 +7,11 @@ import { AuctionsSiteComponent } from './auctions-site/auctions-site.component';
 import { PromotionsSiteComponent } from './promotions-site/promotions-site.component';
 import { AuctionCardComponent } from './auction-card/auction-card.component';
 import { CartSiteComponent } from './cart-site/cart-site.component';
+import { AddAuctionSiteComponent } from './add-auction-site/add-auction-site.component';
 
 
 @NgModule({
-  declarations: [AuctionsSiteComponent, PromotionsSiteComponent, AuctionCardComponent, CartSiteComponent],
+  declarations: [AuctionsSiteComponent, PromotionsSiteComponent, AuctionCardComponent, CartSiteComponent, AddAuctionSiteComponent],
   imports: [
     CommonModule,
     AuctionRoutingModule,
