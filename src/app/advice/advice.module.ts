00dd87be4118e9dd6f7e7be7de8e8b36365f9026
aleksander.gajda <aleksander.gajda@selvita.com>
2020-01-31 09:11:55 +0100

    Add Auction site component

--- src/app/advice/advice.module.ts
+++ src/app/advice/advice.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdviceRoutingModule } from './advice-routing.module';
+import { AdvicesSiteComponent } from './advices-site/advices-site.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [AdvicesSiteComponent],
   imports: [
     CommonModule,
     AdviceRoutingModule
---
+++ src/app/advice/advices-site/advices-site.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ap-advices-site',
+  template: `
+    <p>
+      advices-site works!
+    </p>
+  `,
+  styles: []
+})
+export class AdvicesSiteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
